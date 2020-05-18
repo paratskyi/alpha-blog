081be96221d34810f33f8cef26d8ceee72f1cd99
Bogdan Paratskiy <bog4242@gmail.com>
2020-05-18 17:37:44 +0300

    rubocop autocorrect

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,9 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  
+
   def show; end
-  
+
   def edit; end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_161225) do
-
+ActiveRecord::Schema.define(version: 20_200_515_161_225) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
