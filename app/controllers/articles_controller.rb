5f5ed3dfe8b10fec10a0db687551fb3404c086eb
Bogdan Paratskiy <bog4242@gmail.com>
2020-05-20 22:07:55 +0300

    Add require same user restrictions to users controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -51,9 +51,9 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
-      flash[:alert] = 'You can only edit or delete your own article'
-      redirect_to @article
-    end
+    return if current_user == @article.user
+    
+    flash[:alert] = 'You can only edit or delete your own article'
+    redirect_to @article
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update]
+  before_action :require_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -42,5 +44,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    return if current_user == @user
+
+    flash[:alert] = 'You can only edit your own account'
+    redirect_to @user
   end
 end
