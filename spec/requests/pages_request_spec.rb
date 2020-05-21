3d4778c97aa7c15f89439664370b0357af785369
Bogdan Paratskiy <bog4242@gmail.com>
2020-05-21 18:40:17 +0300

    install rspec

--- spec/requests/pages_request_spec.rb
+++ spec/requests/pages_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Pages', type: :request do
   describe 'GET /home' do
     it 'returns http success' do
-      get '/pages/home'
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
