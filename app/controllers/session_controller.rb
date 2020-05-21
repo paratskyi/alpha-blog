40dc934efc2eaf016fd43b58e7c9de351e03bbf0
Bogdan Paratskiy <bog4242@gmail.com>
2020-05-21 16:26:03 +0300

    Assignment solution: alert message color

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,17 +7,17 @@ class SessionController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = 'Logged in successfully'
+      flash[:success] = 'Logged in successfully'
       redirect_to user
     else
-      flash.now[:alert] = 'There was something wrong with your login details'
+      flash.now[:danger] = 'There was something wrong with your login details'
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = 'Logged out'
+    flash[:success] = 'Logged out'
     redirect_to :root
   end
 end
