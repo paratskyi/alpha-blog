1d4871001bc2cce2f7cfb70f4942a89125a18ff4
Bogdan Paratskiy <bog4242@gmail.com>
2020-06-04 21:19:23 +0300

    Add article category many-to-many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_155057) do
+ActiveRecord::Schema.define(version: 2020_06_04_180706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
